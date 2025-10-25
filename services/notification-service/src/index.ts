f45b59e45b8deb7accbaf6406442872844cc0c74
mkaspulanwar <mkaspulanwar2002@gmail.com>
2025-10-26 07:43:18 +0800

    test(red): add minimal Jest+Supertest for orders & notifications

---
+++ services/notification-service/src/index.ts
@@ -0,0 +1,5 @@
+import express from 'express';
+
+const app = express();
+// BELUM ada route & middleware (sengaja, agar tes RED dulu)
+export default app;
---
+++ services/order-service/src/index.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+// BELUM ada route & middleware (sengaja, agar tes RED dulu)
+export default app;
