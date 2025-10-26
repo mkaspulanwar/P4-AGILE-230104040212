b12829309f81c9608d0c10dde41cfc0d34f4a1cf
mkaspulanwar <mkaspulanwar2002@gmail.com>
2025-10-26 10:27:32 +0800

    all done

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,15 +1,9 @@
-// jest.config.cjs
-
 /** @type {import('jest').Config} */
 const config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  // Hapus atau ganti baris 'roots' ini
-  // roots: ['<rootDir>/services'], 
-  // Jika dihilangkan, Jest akan mencari dari root.
-  // Jika dipertahankan, harus benar-benar di dalam folder services
-  
-  testMatch: ['**/test/**/*.spec.ts'], // Path ini terlihat sudah benar
+  roots: ['<rootDir>/services'],
+  testMatch: ['**/test/**/*.spec.ts'],
   moduleFileExtensions: ['ts', 'js', 'json'],
   verbose: true,
   testTimeout: 10000
